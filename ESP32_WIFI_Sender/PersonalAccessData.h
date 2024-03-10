c9bfdc39eaf964403b74068fa235212756b5b0b9
Sefan1980 <70341174+Sefan1980@users.noreply.github.com>
2024-03-10 13:08:49 +0100

    Update PersonalAccessData.h

--- ESP32_WIFI_Sender/PersonalAccessData.h
+++ ESP32_WIFI_Sender/PersonalAccessData.h
@@ -1,10 +1,10 @@
 
 
 //********************* WLAN Settings **********************************
-const char* ssid = "Sefan WLAN 2,4GHz";                 // put here your acces point ssid -->  If you use the station an an Accesspoint, put the SSID for your AP here. The SSID for AP should not match an existing network.
-const char* password = "StefanJaninaRenkaDeik";         // put here the password - min. 7 chars --> If you use the station as an Accesspoint, put the AP-password here.
+const char* ssid = "WLAN-SSID";                 // put here your acces point ssid -->  If you use the station an an Accesspoint, put the SSID for your AP here. The SSID for AP should not match an existing network.
+const char* password = "PASSWORD";         // put here the password - min. 7 chars --> If you use the station as an Accesspoint, put the AP-password here.
 
 
 //********************* AhatsApp Settings **********************************
-String mobile_number = "491701670959";                  // Type here your phone-number (e.g.: +49 170 123456789 --> 4917012345678)
-String api_key = "77337729";                            // Enter the API-key here
+String mobile_number = "491701234567";                  // Type here your phone-number (e.g.: +49 170 123456789 --> 4917012345678)
+String api_key = "11223344";                            // Enter the API-key here
