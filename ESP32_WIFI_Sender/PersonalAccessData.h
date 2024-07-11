fe71c571a7cae73270ea6922ee0245c8b080526c
Sefan1980 <golfi54@web.de>
2024-07-11 15:50:53 +0200

    Update PersonalAccessData.h

--- ESP32_WIFI_Sender/PersonalAccessData.h
+++ ESP32_WIFI_Sender/PersonalAccessData.h
@@ -1,10 +1,11 @@
 
 
 //********************* WLAN Settings **********************************
-const char* ssid = "Sefan WLAN 2,4GHz";               // SSID for STA mode
-const char* ssidAP = "TeensySenderAP";        // Name of the AccesPoint
-const char* password = "StefanJaninaRenkaDeik";            // min. 7 chars
-const char* passwordAP = "123456789";         // could be blank
+const char* ssid = "SSID";                              // SSID for STA mode
+const char* password = "PASSWORD";                      // min. 7 chars
+
+const char* ssidAP = "TeensySenderAP";                  // Name of the AccesPoint
+const char* passwordAP = "123456789";                   // could be blank
 
 //********************* AhatsApp Settings **********************************
 String mobile_number = "491701234567";                  // Type here your phone-number (e.g.: +49 170 123456789 --> 4917012345678)
