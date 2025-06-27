8c31c088fda07ac027662bad682969d4af074279
Stefan Hagel <70341174+Sefan1980@users.noreply.github.com>
2025-06-27 20:54:22 +0200

    Update config.h

--- ESP32_WiFi_and_BT_Bridge/config.h
+++ ESP32_WiFi_and_BT_Bridge/config.h
@@ -1,60 +1,20 @@
-// use Bluetooth?
-#define BLUETOOTH
-
-// use WiFi?
-#define WiFiAccess
-
-//Select one of the 2 possibilities AP (access point) or STA (station)
-// Just one mode is possible
-#define MODE_AP           // phone connects directly to ESP32 inside the mower - Mower is an accesspoint! 
-//#define MODE_STA        // ESP32 connects to wifi router - Phone has to be connected to the same WiFi!
-
-// use OTAupdates? (just possible with MODE_STA)
-#define OTAupdates
-
-// buffersize
+#define PROTOCOL_TCP
 #define bufferSize 1024
 
-// PFOD port
-const int PFODport = 8881;
-
-// Baudrate to console
-const int consoleBaud = 115200;
-
-// Baudrate to Mower
-const int mowerBaud = 115200;
-
-// Show debug messages in serial console
 bool debug = true;
 
-// version
 #define VERSION "2.00"
 
-
-// If you use Station mode, type in your SSID, PASSWORD, IP, GATEWAY and SUBNETMASK
-#ifdef MODE_STA
-
-// SSID and PASSWORD
-const char *ssid = "Your ssid";       // You will connect your phone to this Access Point
-const char *pw = "Your password";     // and this is the password
+//const char *ssid = "Linda-Lu";         // WiFi SSID from your router
+//const char *pw = "BesteLaune";       // and this is the password
+const char *ssid = "Personal OA";         // WiFi SSID from your router
+const char *pw = "scharbeutz2019";       // and this is the password
 
 //for pfod use this IP and port 8881
-IPAddress ip(192, 168, 178, 122);          // IP for your Mower
-IPAddress gateway(192, 168, 178, 1);       // Gateway from your WiFi, most it's the Router IP
+//IPAddress ip(192, 168, 178, 222);
+//IPAddress gateway(192, 168, 178, 1);
+//IPAddress netmask(255, 255, 255, 0);
+
+IPAddress ip(192, 168, 27, 222);
+IPAddress gateway(192, 168, 27, 1);
 IPAddress netmask(255, 255, 255, 0);
-#endif
-
-
-// Use the following data when using the access point.
-#ifdef MODE_AP
-
-// For AP mode:
-// Don't forget to connect your WiFi phone to the AP
-const char *ssid = "Teensy2";         // You will connect your phone to this Access Point
-const char *pw = "";                  // password
-
-//for pfod use this IP and port 8881
-IPAddress ip(192, 168, 4, 1);
-IPAddress gateway(192, 168, 4, 0);
-IPAddress netmask(255, 255, 255, 0);
-#endif
